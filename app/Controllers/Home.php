bcef9a1619d9d78c4d5367221263249a714b3d22
rikaningtiasazhari <rikaningtiasazhari02@gmail.com>
2020-10-16 23:39:00 +0700

    Update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,5 +8,11 @@ class Home extends BaseController
 	}
 
 	//--------------------------------------------------------------------
-
+public function show(){
+	$data['nama'] = 'Rika Ningtias Azhari';
+	$data['NPM'] = '1817051073';
+	echo view('mahasiswa/header');
+	echo view('mahasiswa/index', $data);
+	echo view('mahasiswa/footer');
+	}
 }
